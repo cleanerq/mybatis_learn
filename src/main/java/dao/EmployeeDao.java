f85e50d46388d03cdce89088bf8947ddcbe687ac
cleanrq <qbyun130@163.com>
2020-07-15 10:50:10 +0800

    import

--- src/main/java/dao/EmployeeDao.java
+++ src/main/java/dao/EmployeeDao.java
@@ -5,4 +5,10 @@ import bean.Employee;
 public interface EmployeeDao {
     //根据员工ID查找
     public Employee getEmpById(Integer id);
+
+    public int updateEmployee(Employee employee);
+
+    public boolean deleteEmployee(Integer id);
+
+    public int insertEmployee(Employee employee);
 }
--- src/test/java/dao/EmployeeDaoTest.java
+++ src/test/java/dao/EmployeeDaoTest.java
@@ -6,14 +6,19 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-public class EmployeeDaoTest extends TestCase {
-    @Test
-    public void testGetEmpById() {
+public class EmployeeDaoTest {
+    private EmployeeDao employeeDao;
+    private SqlSession sqlSession;
+
+    @Before
+    public void beforeTest() {
         // 根据全局配置文件创建出一个 SqlSessionFactory
         // sqlSessionFactory 是sqlSession工程 负责创建SqlSession对象
         // SqlSession sql会话 代表和数据库的一次会话
@@ -25,16 +30,47 @@ public class EmployeeDaoTest extends TestCase {
         }
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         // 获取和数据库的一次会话
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        sqlSession = sqlSessionFactory.openSession();
+        employeeDao = sqlSession.getMapper(EmployeeDao.class);
+    }
+
+    @After
+    public void afterTest() {
+        sqlSession.close();
+    }
+
+
+    @Test
+    public void testGetEmpById() {
         try {
-            EmployeeDao employeeDao = sqlSession.getMapper(EmployeeDao.class);
             Employee empById = employeeDao.getEmpById(1);
             System.out.println(empById);
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            sqlSession.close();
         }
-
     }
+
+    @Test
+    public void testUpdateEmployee() {
+        Employee employee = new Employee();
+        employee.setEmpName("admin123");
+        employee.setId(1);
+        employeeDao.updateEmployee(employee);
+    }
+
+
+    @Test
+    public void testInsertEmployee() {
+        Employee employee = new Employee();
+        employee.setEmpName("lisi");
+        employee.setEmail("lisi@qq.com");
+        employee.setGender(1);
+        employeeDao.insertEmployee(employee);
+    }
+
+    @Test
+    public void testDeleteEmployee() {
+        employeeDao.deleteEmployee(2);
+    }
+
 }
