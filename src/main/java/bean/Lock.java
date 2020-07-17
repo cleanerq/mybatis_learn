04f75f87e2ed8ad270254dd8f482752bff9ce83e
cleanrq <qbyun130@163.com>
2020-07-17 17:30:06 +0800

    import

--- src/main/java/bean/Lock.java
+++ src/main/java/bean/Lock.java
@@ -1,11 +1,24 @@
 package bean;
 
+import java.util.List;
+
 /**
  * 锁
  */
 public class Lock {
     private Integer id;
     private String lockName;
+    // 查询锁时把所有的钥匙放到集合里面
+    private List<Key> keys;
+
+
+    public List<Key> getKeys() {
+        return keys;
+    }
+
+    public void setKeys(List<Key> keys) {
+        this.keys = keys;
+    }
 
     public Integer getId() {
         return id;
@@ -28,6 +41,7 @@ public class Lock {
         return "Lock{" +
                 "id=" + id +
                 ", lockName='" + lockName + '\'' +
+                ", keys=" + keys +
                 '}';
     }
 }
